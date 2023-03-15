fbf73a90beff731e167e52dfc48a52b5fb15adcd
Michele Dorph De Chiffre <md@autonik.dk>
2023-03-15 15:04:51 +0100

    Risolve non rediretto dopo token exchange

--- src/routes/oauth-callback.tsx
+++ src/routes/oauth-callback.tsx
@@ -1,14 +1,17 @@
 // import { BASEPATH } from '$lib/base-path';
-
 import { redirect } from "solid-start";
 import oauthclient, { autoLogin } from "../lib/oauth";
-if (typeof window != "undefined" && window.location.search.indexOf("code=") !== -1) {
-    oauthclient.exchangeAuthCode().then(() => { 
-        autoLogin();
-        redirect('/');
-    });
-}
+import {onMount} from "solid-js";
 
 export default function OAuthCallback() {
+    onMount(() => {
+        if (typeof window != "undefined" && window.location.search.indexOf("code=") !== -1) {
+            oauthclient.exchangeAuthCode().then(() => {
+                autoLogin();
+                location.href = '/';
+            });
+        }
+    });
+
     return <h1>Exchanges OAuth credentials</h1>;
 }
