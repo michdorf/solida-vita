f2d40b6e18824ce82fee6e452eab05d3c8118839
Michele Dorph De Chiffre <md@autonik.dk>
2023-03-15 15:19:33 +0100

    Implementa nota component

--- src/components/Nota.tsx
+++ src/components/Nota.tsx
@@ -1,5 +1,13 @@
-export default Nota() {
+import NotaT from "~/interface/nota";
+import {Show} from "solid-js";
+
+export default function Nota(params: {nota: NotaT | undefined}) {
     return (
-        <h1>{notaSelto()?.titolo}</h1>
+        <>
+        <Show when={params.nota?.titolo} fallback={<h1>No title</h1>}>
+            <h1>{params.nota?.titolo}</h1>
+        </Show>
+        <textarea placeholder="Cosa hai in mente?" class="contenuto" style="font-size: 20px; height: 168px" onkeyup="handleTextAreaHeight($event)" onfocus="handleTextAreaHeight($event)">{params.nota?.contenuto}</textarea><br/>
+        </>
     )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,17 @@
 import "./index.css";
 import {createEffect, createSignal, For, Show} from "solid-js";
-import initMemo from "~/lib/db"; '../lib/db'
+import initMemo from "~/lib/db";
 import oauthclient, { autoLogin, oauthStatus } from "~/lib/oauth";
 import Memo from "~/moduli/memo/memo";
 import note from "~/stores/note";
 import NotaVoce from "~/components/NotaVoce";
-import Nota from "~/interface/nota";
+import Nota from "~/components/Nota";
+import NotaT from "~/interface/nota";
 import {isServer} from "solid-js/web";
 
 let memo: Memo;
 export default function Home() {
-  const [notaSelto, setNotaSelto] = createSignal<Nota>();
+  const [notaSelto, setNotaSelto] = createSignal<NotaT>();
 
   if (!isServer) {
     try {
@@ -35,7 +36,7 @@ export default function Home() {
         </div>
         <div class="right panel">
           <Show when={notaSelto()}>
-
+            <Nota nota={notaSelto()} />
           </Show>
           <Show when={oauthStatus() !== "authorized"}>
             <button onclick={login}>Login</button>
