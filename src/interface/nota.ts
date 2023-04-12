8c23097981ac700d746748df005a6ce40d0e7a88
Michele de Chiffre <michele@dechiffre.dk>
2023-04-12 14:47:43 +0200

    Typing coretto di NotaI

--- src/interface/nota.ts
+++ src/interface/nota.ts
@@ -2,7 +2,10 @@ import IMemoRiga from "~/moduli/memo/memoriga.interface";
 
 export default interface INota extends IMemoRiga {
     titolo:string;
+    pinned: boolean;
     contenuto: string;
+    ricorda: string;
+    quaderno: string;
     d_time: number;
     enc_versione: number;
 }
