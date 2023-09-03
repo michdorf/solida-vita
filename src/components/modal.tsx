106560d9804cadf70f3884465d29b019f0d827ee
Michele de Chiffre <michele@dechiffre.dk>
2023-09-03 09:55:36 +0200

    Agg. modo di chiudere il modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -2,14 +2,19 @@ import { ParentProps, Show, createEffect, createSignal, onMount } from "solid-js
 import { Portal } from "solid-js/web";
 
 export default function Modal(props: ParentProps & {show: boolean}) {
-    const [hide, setHide] = createSignal(true);
     const [modal, setModal] = createSignal<HTMLElement | null>(null);
     createEffect(() => {
         if (props.show && modal()) {
-            setHide(props.show);
             modal()!.style.display = props.show ? "block" : "none";
         }
     });
+
+    function hide() {
+        const el = modal();
+        if (el) {
+            el.style.display = "none";
+        }
+    }
 
     onMount(() => {
         setModal(document.getElementById("modal"));
@@ -19,7 +24,7 @@ export default function Modal(props: ParentProps & {show: boolean}) {
         <>
         <Show when={modal() && props.show}>
         <Portal mount={modal()!}>
-            <button onClick={() => setHide(true)}>Luk</button>
+            <button onClick={hide}>Luk</button>
             {props.children}
         </Portal>
         </Show>
