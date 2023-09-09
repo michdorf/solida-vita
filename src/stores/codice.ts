f9a68db509613200c450a7c6f16299742f8c258c
Michele de Chiffre <michele@dechiffre.dk>
2023-09-09 11:36:32 +0200

    Risolve nota selezionata ricordato su decrypt

--- src/stores/codice.ts
+++ src/stores/codice.ts
@@ -1,15 +1,18 @@
 import {createSignal} from "solid-js";
+import { useNavigate } from "solid-start";
 declare let cifra: any;
 
 const [codice, setCodice] = createSignal("");
 const [cifErrore, setCifErrore] = createSignal(false);
 
 export function decrypt(input: string, enc_versione?: number) {
+    const navigate = useNavigate();
+
     if (!enc_versione) {
         return input;
     }
     if (!codice()) {
-        location.href = "/unlock";
+        navigate("/unlock");
         return "";
     } else {
         const dez = cifra.dec(input, codice(), enc_versione);
@@ -24,11 +27,13 @@ export function decrypt(input: string, enc_versione?: number) {
 }
 
 export function encrypt(input: string, enc_versione: number = 2) {
+    const navigate = useNavigate();
+
     if (!enc_versione) {
         return input;
     }
     if (!codice()) {
-        location.href = "/unlock";
+        navigate("/unlock");
         return "";
     } else {
         return cifra.enc(input, codice(), enc_versione);
